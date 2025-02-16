92436ddea2255cdc2beea717b8f07ffa11890671
JosiahAgency <mwandajosiah@gmail.com>
2025-02-16 18:46:07 +0300

    Clerk Setup Auth Complete

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up', '/'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/'])
 
-export default clerkMiddleware((auth, req) => {
-    if (!isPublicRoute(req)) auth.protect()
-})
+export default clerkMiddleware(async (auth, req) => {
+    console.log('Testing testing')
+    if (!isPublicRoute(req)) await auth.protect();
+});
 
 export const config = {
-    matcher: [
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        '/(api|trpc)(.*)',
-    ],
-}
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
